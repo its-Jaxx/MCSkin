9dc807a29bf2560ba9742775941a95f47a98ef8a
jaxx <96382604+its-Jaxx@users.noreply.github.com>
2023-03-24 23:40:02 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,73 @@
 import discord
 import requests
 
+with open('config.txt', 'r') as f:
+  prefix = f.read().strip()
+
 intents = discord.Intents.all()
 intents.members = True
 client = discord.Client(intents=intents)
 
+
 @client.event
 async def on_ready():
-    print(f"Logged in as {client.user.name}")
-    print("Bot is ready to use")
+  print(f"Logged in as {client.user.name}")
+  print("Bot is ready to use")
+
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!skin'):
-        username = message.content[6:]
-        response = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{username}")
-        if response.status_code == 204:
-            embed = discord.Embed(title="Error", description=f"Unable to find player UUID for user {username}", color=discord.Color.red())
-            await message.channel.send(embed=embed)
-            return
-        
-        try:
-            uuid = response.json()['id']
-        except KeyError:
-            embed = discord.Embed(title="Error", description=f"Unable to find player UUID for user {username}", color=discord.Color.red())
-            await message.channel.send(embed=embed)
-            return
-        
-        skin_url = f"https://crafatar.com/renders/body/{uuid}"
-        
-        embed = discord.Embed(title=f"Skin for user {username}")
-        embed.set_image(url=skin_url)
-        await message.channel.send(embed=embed)
-        
-        print(f"Received message: {message.content}")
-        print(f"Parsed username: {username}")
-        print(f"Retrieved UUID: {uuid}")
-        print(f"Retrieved skin URL: {skin_url}")
-        print("Message sent successfully")
-    elif message.content.startswith('!ping'):
-        await message.channel.send("Pong!")
-        print("Ping-pong!")
+  global prefix
+  if message.content.startswith(prefix + 'skin'):
+    if len(message.content.split()) < 2:
+      embed = discord.Embed(title="Error",
+                            description="Please provide a Minecraft username",
+                            color=discord.Color.red())
+      await message.channel.send(embed=embed)
+      return
 
-client.run("put ur bot token here :D")
+    username = message.content[6:]
+    response = requests.get(
+      f"https://api.mojang.com/users/profiles/minecraft/{username}")
+    if response.status_code == 204:
+      embed = discord.Embed(
+        title="Error",
+        description=f"Unable to find player UUID for user {username}",
+        color=discord.Color.red())
+      await message.channel.send(embed=embed)
+      return
+
+    try:
+      uuid = response.json()['id']
+    except KeyError:
+      embed = discord.Embed(
+        title="Error",
+        description=f"Unable to find player UUID for user {username}",
+        color=discord.Color.red())
+      await message.channel.send(embed=embed)
+      return
+
+    skin_url = f"https://crafatar.com/renders/body/{uuid}"
+
+    embed = discord.Embed(title=f"Skin for user {username}")
+    embed.set_image(url=skin_url)
+    await message.channel.send(embed=embed)
+
+    print(f"Received message: {message.content}")
+    print(f"Parsed username: {username}")
+    print(f"Retrieved UUID: {uuid}")
+    print(f"Retrieved skin URL: {skin_url}")
+    print("Message sent successfully")
+  elif message.content.startswith(prefix + 'ping'):
+    await message.channel.send("Pong!")
+    print("Ping-pong!")
+  elif message.content.startswith(prefix + 'changeprefix'):
+    new_prefix = message.content.split()[1]
+    with open('config.txt', 'w') as f:
+      f.write(new_prefix)
+    await message.channel.send(f'Prefix changed to {new_prefix}')
+    prefix = new_prefix
+
+
+client.run(
+  "Put your token ID here:")
