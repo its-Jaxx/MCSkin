ce38ef2a088b78dfbb079b48097fa445cefe5164
jaxx <96382604+its-Jaxx@users.noreply.github.com>
2023-03-30 19:50:59 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,72 +1,66 @@
 import discord
 import requests
 import datetime
-
-with open('config.txt', 'r') as f:
-  prefix = f.read().strip()
+from discord.ext import commands
+from discord.ext.commands import BucketType
 
 intents = discord.Intents.all()
 intents.members = True
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='+', intents=intents)
 
+default_cooldown = commands.CooldownMapping.from_cooldown(1, 5, commands.BucketType.user)
+bot.default_cooldown = default_cooldown
 
-@client.event
+@bot.event
 async def on_ready():
-  print(f"Logged in as {client.user.name}")
-  print("Bot is ready to use")
+    print(f"Logged in as {bot.user.name}")
+    print("Bot is ready to use")
+    
+@bot.command()
+async def skin(ctx, *, username=None):
+    if not username:
+        embed = discord.Embed(
+            title="Error",
+            description="Please provide a Minecraft username",
+            color=discord.Color.red())
+        await ctx.send(embed=embed)
+        return
 
-
-@client.event
-async def on_message(message):
-  global prefix
-  if message.content.startswith(prefix + 'skin'):
-    if len(message.content.split()) < 2:
-      embed = discord.Embed(title="Error",
-                            description="Please provide a Minecraft username",
-                            color=discord.Color.red())
-      await message.channel.send(embed=embed)
-      return
-
-    username = message.content[6:]
-    response = requests.get(
-      f"https://api.mojang.com/users/profiles/minecraft/{username}")
+    response = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{username}")
     if response.status_code == 204:
-      embed = discord.Embed(
-        title="Error",
-        description=f"Unable to find player UUID for user {username}",
-        color=discord.Color.red())
-      await message.channel.send(embed=embed)
-      return
-
+        embed = discord.Embed(title="Error", color=discord.Color.red())
+        embed.add_field(value=f"Unable to find player UUID for user {username}")
+        embed.add_field(value=f"Make sure you spelled it correctly")
+        await ctx.send(embed=embed)
+        return
+    
     try:
-      uuid = response.json()['id']
+        uuid = response.json()['id']
     except KeyError:
-      embed = discord.Embed(
-        title="Error",
-        description=f"Unable to find player UUID for user {username}",
-        color=discord.Color.red())
-      await message.channel.send(embed=embed)
-      return
+        embed = discord.Embed(title="Error", color=discord.Color.red())
+        embed.add_field(name="", value=f"Unable to find player UUID for user {username}", inline=False)
+        embed.add_field(name="", value=f"Make sure you spelled it correctly", inline=False)
+        await ctx.send(embed=embed)
+        return
 
     skin_url = f"https://crafatar.com/renders/body/{uuid}"
     model_url = f"https://crafatar.com/skins/{uuid}"
 
     embed = discord.Embed(title=f"Skin for user {username}")
     embed.set_image(url=skin_url)
-    embed.add_field(name="",
-                    value=f"[Click to download template]({model_url})",
-                    inline=False)
-    await message.channel.send(embed=embed)
+    embed.add_field(name="", value=f"[Click to download template]({model_url})", inline=False)
+    await ctx.send(embed=embed)
 
-    print(f"Received message: {message.content}")
+    print(f"Received message: {ctx.message.content}")
     print(f"Parsed username: {username}")
     print(f"Retrieved UUID: {uuid}")
     print(f"Retrieved skin URL: {skin_url}")
     print("Message sent successfully")
 
-  elif message.content.startswith(prefix + 'ping'):
+@bot.command()
+async def ping(ctx):
     start_time = datetime.datetime.now()
-    message_sent = await message.channel.send("Pinging...")
+    message_sent = await ctx.send("Pinging...")
     end_time = datetime.datetime.now()
 
     latency = end_time - start_time
@@ -74,18 +68,29 @@ async def on_message(message):
 
     await message_sent.edit(content=f"Pong! Latency: {ping_time} ms")
     print("Ping-pong!")
+    
+@bot.command()
+async def commands(ctx):
+    skin = "Grabs a model of the skin"
+    ping = "Pings the server to retrieve latency in ms"
+    mention1 = "@mention"
+    mention2 = "Ping the bot to get the current prefix"
+    commandlist = "Shows this list of commands"
 
-  elif client.user.mentioned_in(message):
-    await message.reply(f"My prefix is {prefix}")
+    embed = discord.Embed(title="Commands List", color=discord.Color.green())
+    embed.add_field(name="+skin", value=f"{skin}", inline=False)
+    embed.add_field(name="+ping", value=f"{ping}", inline=False)
+    embed.add_field(name=f"{mention1}", value=f"{mention2}", inline=False)
+    embed.add_field(name="+command", value=f"{commandlist}", inline=False)
 
-  elif message.content.startswith(prefix + 'changeprefix'):
-    if message.author.guild_permissions.administrator:
-      new_prefix = message.content.split()[1]
-      with open('config.txt', 'w') as f:
-        f.write(new_prefix)
-      await message.channel.send(f'Prefix changed to {new_prefix}')
-      prefix = new_prefix
-    else:
-      await message.channel.send(
-        "You don't have the permission to change the prefix.")
-client.run("")
+    message_sent = await ctx.send(embed=embed)
+    print("Commands list being printed")
+
+@bot.event
+async def on_message(message):
+    if bot.user.mentioned_in(message):
+        await message.reply(f"My prefix is `+`")
+    
+    await bot.process_commands(message)
+
+bot.run("Enter bot token here")
